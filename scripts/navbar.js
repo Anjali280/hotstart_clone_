6dd427b1433dd71f55e2e6b0c0406b2e55860c5d
Anjali280 <mondalanjali0707@gmail.com>
2022-10-19 16:56:12 +0530

    mixed content error corrected

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -15,61 +15,46 @@
             <button class="sub">Subscribe</button>
             <button class="log">LOGIN</button>
         </div> */
-        
 
-//   const input=document.querySelector(".input").value 
-  const suggestion=document.querySelector(".search-drop").value
-  const maindiv=document.querySelector("#ash").value
+//   const input=document.querySelector(".input").value
+const suggestion = document.querySelector(".search-drop").value;
+const maindiv = document.querySelector("#ash").value;
 
+const url = "https://abhishek6416.github.io/hotstarapii/db.json";
 
-  const url="https://abhishek6416.github.io/hotstarapii/db.json"
+async function display(url) {
+  try {
+    const input = document.querySelector(".input").value;
+    const a1 = await fetch(url);
+    const a2 = await a1.json();
+    // display1(a2.moviedata.trending)
+    console.log(a2.moviedata);
+    // input.onkeyup=(el)=>{
+    //     alert("yoyo")
 
-  async function display(url){
-    try {
-        const input=document.querySelector(".input").value 
-        const a1=await fetch(url)
-        const a2=await a1.json()
-        // display1(a2.moviedata.trending)
-        console.log(a2.moviedata)
-        // input.onkeyup=(el)=>{
-        //     alert("yoyo")
+    //     console.log(el.target.value)
 
-        //     console.log(el.target.value)
+    // }
+    input.onkeyup = (el) => {
+      let elem = el.target.value;
+      console.log(el);
+      let array = a2.moviedata.movies;
 
-        // }
-            input.onkeyup=(el)=>{
-                let elem=el.target.value;
-                console.log(el)
-                let array=a2.moviedata.movies
-    
-            
-
-
-        if(el.target.value){
-            console.log(elem)
-            array.filter((el)=>{
-    
-                if(el.title[0].toLowerCase()===elem){
-                    console.log(el)
-                }
-                // console.log(el)
-                    // return el;
-    
-            })
-        }
-        }
+      if (el.target.value) {
+        console.log(elem);
+        array.filter((el) => {
+          if (el.title[0].toLowerCase() === elem) {
+            console.log(el);
+          }
+          // console.log(el)
+          // return el;
+        });
+      }
+    };
     //    fun(a2.moviedata)
-
-
-
-
-    } catch (error) {
-        
-    }
-  }
-  display(url)
-
-
+  } catch (error) {}
+}
+display(url);
 
 //   document.querySelector(".input").addEventListener("keyup",fun)
 
@@ -79,13 +64,7 @@
 // //    console.log(data)
 //     let elem=event.target.value;
 //     let array=data.movies
-    
-    
-    
+
 //     console.log(elem);
 //     // alert("yo yo")
 //   }
-
-  
-  
-  
